f1a34b829d21e5bcbeed1ca19140491f9287b2fd
atxpaul <pablo.pan.veira@gmail.com>
2021-11-07 01:02:32 +0100

    Added persistence on fileStorage and first steps on server.js

---
+++ src/persistence/container.js
@@ -0,0 +1,103 @@
+import fs from 'fs';
+
+class Container {
+  constructor(fileName) {
+    this.fileName = fileName;
+    const nombres = fs.readdirSync(__dirname);
+    if (nombres.indexOf(fileName) == -1) {
+      fs.writeFileSync(fileName, '');
+    }
+  }
+
+  async save(object) {
+    let id;
+    let content = await this.getAll();
+    if (content.length > 0) {
+      let ids = content.map((c) => c.id);
+      id = Math.max(...ids) + 1;
+    } else {
+      id = 1;
+      content = [];
+    }
+    object.id = id;
+    content.push(object);
+    const json = JSON.stringify(content, null, 4);
+    try {
+      await fs.promises.writeFile(this.fileName, json, 'utf-8');
+    } catch (err) {
+      console.log(err);
+    }
+    return id;
+  }
+
+  async updateById(id, newObject) {
+    let object;
+    let content = await this.getAll();
+    try {
+      object = content.find((c) => c.id == id);
+    } catch (err) {
+      console.log(err);
+    }
+    if (!object) {
+      return [];
+    }
+    content = content.filter((c) => c.id !== id);
+
+    newObject.id = object.id;
+
+    content.push(newObject);
+    const json = JSON.stringify(content, null, 4);
+    try {
+      await fs.promises.writeFile(this.nombreArchivo, json, 'utf-8');
+    } catch (err) {
+      console.log(err);
+    }
+
+    return object ? object : [];
+  }
+
+  async getById(id) {
+    let object;
+    let content = await this.getAll();
+    try {
+      object = content.find((c) => c.id == id);
+    } catch (err) {
+      console.log(err);
+    }
+
+    return object ? object : [];
+  }
+
+  async getAll() {
+    let content;
+    try {
+      content = await fs.promises.readFile(this.fileName, 'utf-8');
+    } catch (err) {}
+    if (content) {
+      return JSON.parse(content);
+    } else {
+      return [];
+    }
+  }
+
+  async deleteById(id) {
+    let content = await this.getAll();
+    content = content.filter((c) => c.id !== id);
+    const json = JSON.stringify(content, null, 4);
+    try {
+      await fs.promises.writeFile(this.fileName, json, 'utf-8');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(this.fileName, '', 'utf-8');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+export default Container;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
-const express = require('express');
+import express from 'express';
+import productRouter from './router/productRouter';
+import cartRouter from './router/cartRouter';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/products', productRouter);
+app.use('/api/cart', cartRouter);
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on('error', (error) => console.log(`Error en servidor ${error}`));
