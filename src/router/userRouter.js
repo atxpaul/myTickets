0a5e5d8c1be7288eba39a01cc64fa004f66d738e
atxpaul <pablo.pan.veira@gmail.com>
2022-02-07 09:35:59 +0100

    Split passport to middleware

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,10 +1,6 @@
 import express from 'express';
-import passport from 'passport';
+import passport from '../middleware/Passport.js';
 import session from 'express-session';
-import bCrypt from 'bcrypt';
-import LocalStrategy from 'passport-local';
-import User from '../model/UserModel.js';
-import logger from '../config/logger.js';
 import mongoose from 'mongoose';
 
 import controller from '../controller/UserController.js';
@@ -26,82 +22,5 @@ router.post(
   passport.authenticate('login', { failureRedirect: '/faillogin' }),
   userController.login
 );
-
-//MIDDLEWARE
-passport.use(
-  'signup',
-  new LocalStrategy(
-    {
-      passReqToCallback: true,
-    },
-    (req, username, password, done) => {
-      logger.info(`Registering user ${username}`);
-      User.findOne({ username: username }, function (err, user) {
-        if (err) {
-          logger.error(`Error in SignUp: ${err}`);
-          return done(err);
-        }
-
-        if (user) {
-          logger.error('User already exists');
-          return done(null, false);
-        }
-
-        const newUser = {
-          username: username,
-          password: createHash(password),
-          email: req.body.email,
-        };
-
-        User.create(newUser, (err, userWithId) => {
-          if (err) {
-            logger.error(`Error in saving user: ${err}`);
-            return done(err);
-          }
-          logger.info(`User registration succesful ${user}`);
-          return done(null, userWithId);
-        });
-      });
-    }
-  )
-);
-
-passport.use(
-  'login',
-  new LocalStrategy((username, password, done) => {
-    logger.info(`Login user ${username}`);
-    User.findOne({ username }, (err, user) => {
-      if (err) return done(err);
-
-      if (!user) {
-        logger.error('User Not Found with username ' + username);
-        return done(null, false);
-      }
-
-      if (!isValidPassword(user, password)) {
-        logger.error('Invalid Password');
-        return done(null, false);
-      }
-
-      return done(null, user);
-    });
-  })
-);
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, done);
-});
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-function isValidPassword(user, password) {
-  return bCrypt.compareSync(password, user.password);
-}
-
-function createHash(password) {
-  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-}
 
 export default router;
