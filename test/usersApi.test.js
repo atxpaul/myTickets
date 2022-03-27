ecc951e8f4c90eab982348b7f480eff266630a69
atxpaul <pablo.pan.veira@gmail.com>
2022-03-27 16:22:58 +0200

    Added user tests, pending to add signup case

---
+++ test/usersApi.test.js
@@ -0,0 +1,105 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+import app from '../src/loader/app.js';
+import path from 'path';
+
+let request;
+let server;
+
+const userSignup = {
+  username: 'test@test',
+  password: '$2b$10$qLc7gIRrhZFR2hmdPimeieaL13h5TgGCyIB6NIG/Z1g.BXIsbzVLu',
+  name: 'Test',
+  surname: 'Test',
+  address: 'Street',
+  age: 32,
+  phone: '666666666',
+};
+
+const userLogin = {
+  username: 'pablo@pablo',
+  password: '1234',
+};
+
+let jwt;
+
+describe('Ciclo Test Users', () => {
+  before(async function () {
+    server = await startServer();
+    request = supertest(`http://localhost:${server.address().port}/api/users`);
+  });
+
+  after(function () {
+    server.close();
+  });
+
+  //   describe('USER SIGNUP', () => {
+  //     it('Signup user and get bearer token', async () => {
+  //       const response = await request
+  //         .post('/signup')
+  //         .field('username', userSignup.username)
+  //         .field('password', userSignup.password)
+  //         .field('name', userSignup.name)
+  //         .field('surname', userSignup.surname)
+  //         .field('address', userSignup.address)
+  //         .field('age', userSignup.age)
+  //         .field('phone', userSignup.phone)
+  //         .attach('profilePic', 'uploads/linkedin.jpg');
+
+  //       expect(response.status).to.eql(200);
+
+  //       const user = response.body;
+  //       jwt = `Bearer ${user.jwt}`;
+  //       expect(user).to.include.keys('username', 'jwt');
+  //       expect(user.username).to.eql(userLogin.username);
+  //       expect(user.jwt).to.have.lengthOf.above(10);
+  //     });
+  //   });
+
+  describe('USER LOGIN', () => {
+    it('Login user and get bearer token', async () => {
+      const response = await request.post('/login').send(userLogin);
+      expect(response.status).to.eql(200);
+
+      const user = response.body;
+      jwt = `Bearer ${user.jwt}`;
+      expect(user).to.include.keys('username', 'jwt');
+      expect(user.username).to.eql(userLogin.username);
+      expect(user.jwt).to.have.lengthOf.above(10);
+    });
+  });
+
+  describe('USER ACCESS', () => {
+    it('User trying to access to protected route with jwt', async () => {
+      const response = await request
+        .get('/home')
+        .send(userLogin)
+        .set('Authorization', jwt);
+      expect(response.status).to.eql(200);
+
+      const home = response.body;
+      expect(home).to.include.keys('user');
+      expect(home.user).to.eql(userLogin.username);
+    });
+    it('User trying to access to protected route without jwt', async () => {
+      const response = await request.get('/home').send(userLogin);
+      expect(response.status).to.eql(401);
+    });
+  });
+});
+
+async function startServer() {
+  return new Promise((resolve, reject) => {
+    const PORT = 0;
+    const server = app.listen(PORT, () => {
+      console.log(
+        `Servidor express escuchando en el puerto ${server.address().port}`
+      );
+      resolve(server);
+    });
+    server.on('error', (error) => {
+      console.log(`Error en Servidor: ${error}`);
+      reject(error);
+    });
+  });
+}
